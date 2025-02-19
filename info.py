d8bdff1b2e2b5baeb007e761c8fb71924dbf58e5
anuj26052006 <anujkumar26052006@gmail.com>
2025-02-19 18:27:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,8 +21,8 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7623594087:AAGqrkqdAqfu2F4sbNSXYM1IRy9dSsx
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1286101018').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/GwAnuj") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002498951039'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+sQ-aUdxHLYJmODQ1')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002275175094').split()]
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Rashmika_allmovie_bot')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002416632662').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Anuj1980:Anuj1980@cluster0.h2a8m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -39,7 +39,7 @@ auth_channel = environ.get('AUTH_CHANNEL', '')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
 request_channel = environ.get('REQUEST_CHANNEL', '0')
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link ( make sure bot is admin )
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/GwAnuj') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', True)
@@ -48,12 +48,12 @@ TUTORIAL = environ.get("TUTORIAL", "https://t.me/EliteModAPKZone/393")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
 SHORTENER_API = environ.get("SHORTENER_API", "ffaf51155ff72b4564e4ede738624783fd37e6dd")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'anylinks.in')
-SHORTENER_API2 = environ.get("SHORTENER_API2", "")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", '')
-SHORTENER_API3 = environ.get("SHORTENER_API3", "")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", '')
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
+SHORTENER_API2 = environ.get("SHORTENER_API2", "ffaf51155ff72b4564e4ede738624783fd37e6dd")
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'anylinks.in')
+SHORTENER_API3 = environ.get("SHORTENER_API3", "ffaf51155ff72b4564e4ede738624783fd37e6dd")
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'anylinks.in')
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "1440"))
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "1440"))
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi"]
@@ -83,12 +83,12 @@ IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', False)
 PORT = environ.get('PORT', '5000')
 MAX_BTN = int(environ.get('MAX_BTN', '8'))
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
-DELETE_TIME = int(environ.get('DELETE_TIME', 1200))
-IMDB = is_enabled('IMDB', False)
+DELETE_TIME = int(environ.get('DELETE_TIME', 300))
+IMDB = is_enabled('IMDB', True)
 FILE_CAPTION = environ.get('FILE_CAPTION', f'{script.FILE_CAPTION}')
 IMDB_TEMPLATE = environ.get('IMDB_TEMPLATE', f'{script.IMDB_TEMPLATE_TXT}')
-LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', False)
-PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
+LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', True)
+PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', True)
 SPELL_CHECK = is_enabled('SPELL_CHECK', True)
 LINK_MODE = is_enabled('LINK_MODE', True)
 
